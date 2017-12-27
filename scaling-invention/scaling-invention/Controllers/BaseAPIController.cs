0816e425f933a8a697f0d4792b4bb45b4a4d301c
Keith <keithtmccartney@hotmail.com>
2017-12-27 17:42:44 +0000

    Adding Base and User Controllers.

---
+++ scaling-invention/scaling-invention/Controllers/BaseAPIController.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using scaling_invention.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace scaling_invention.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+        protected readonly CommonEntities Common = new CommonEntities();
+
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+
+            return response;
+        }
+    }
+}
---
+++ scaling-invention/scaling-invention/Controllers/UserAPIController.cs
@@ -0,0 +1,40 @@
+﻿using scaling_invention.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace scaling_invention.Controllers
+{
+    public class UserAPIController : BaseAPIController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(Common.Users.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody]User value)
+        {
+            Common.Users.Add(value);
+
+            return ToJson(Common.SaveChanges());
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]User value)
+        {
+            Common.Entry(value).State = EntityState.Modified;
+
+            return ToJson(Common.SaveChanges());
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            Common.Users.Remove(Common.Users.FirstOrDefault(x => x.Id == id));
+
+            return ToJson(Common.SaveChanges());
+        }
+    }
+}
